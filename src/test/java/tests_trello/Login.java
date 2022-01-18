0915c9def800e3a8ee73228d2b181da6d261ce20
IvanAlekseevv <kropivnisk@gmail.com>
2022-01-18 21:54:26 +0200

    AppManager

---
+++ src/test/java/tests_trello/Login.java
@@ -0,0 +1,44 @@
+package tests_trello;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Login extends TestBase{
+
+   //  @BeforeMethod
+   // public void preConditions() {
+    //    if (isLogged()) {
+   //         logout();
+     //   }
+ //   }
+
+    @Test
+    public void LoginTest2() throws InterruptedException {
+        app.getUser().initLogin();
+        app.getUser().fillLoginForm();
+        app.getUser().submitLogin();
+        app.getUser().isLoggedSuccess();
+        Assert.assertTrue(app.getUser().isLoggedSuccess());
+        //Thread.sleep(5000);
+    }
+
+
+
+  //  @Test
+  //  public void LoginTest3() {
+   //     click(By.cssSelector("[href='/login']"));
+   //     type(By.cssSelector("#user"), "aleksivan060@gmail.com");
+   //     click(By.cssSelector("#login"));
+   //     click(By.cssSelector(".dlMUdg"));
+   //     type(By.cssSelector("#password"), "20111978");
+    //    click(By.cssSelector("#login-submit"));
+ //   }
+
+
+
+
+}
---
+++ src/test/java/tests_trello/TestBase.java
@@ -0,0 +1,29 @@
+package tests_trello;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import manager.ApplicationManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    protected static ApplicationManager app = new ApplicationManager();
+
+    @BeforeMethod
+    public void setUp() {
+        app.init();
+    }
+
+
+
+    @AfterMethod
+    public void tearDown(){
+        app.stop();
+    }
+
+}
